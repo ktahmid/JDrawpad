89bfccfb5f01ac52a1a633d8f51c3be3aa48aaa4
Tahmid A. Khan <60913202+ktahmid@users.noreply.github.com>
2021-01-10 02:36:35 +0600

    Change tool buttons to icons.

--- src/SvgGenerator.java
+++ src/SvgGenerator.java
@@ -17,14 +17,14 @@ public class SvgGenerator {
 
     public static String convert(Line line) {
         return "<path d=\"" +
-                " M " + line.getStartX() + "," + line.getStartY() +
+                "M " + line.getStartX() + "," + line.getStartY() +
                 " L " + line.getEndX() + "," + line.getEndY() +
                 "\" stroke-width=\"" + line.getStrokeWidth() + "px\"" +
                 " stroke=\"black\" fill=\"none\" />\n";
     }
 
     public static String convert(Polyline polyline) {
-        String path = "<path d=\" M " + polyline.getPoints().get(0) + "," + polyline.getPoints().get(1);
+        String path = "<path d=\"M " + polyline.getPoints().get(0) + "," + polyline.getPoints().get(1);
         for (int i = 2; i < polyline.getPoints().size(); i += 2) {
             path += " L " + polyline.getPoints().get(i);
             path += "," + polyline.getPoints().get(i + 1);
@@ -40,7 +40,7 @@ public class SvgGenerator {
         double aY = mY + (arc.getStartAngle() <= 90 ? -arc.getRadiusY() : arc.getRadiusY());
         double mX = aX + (arc.getStartAngle()==90 || arc.getStartAngle()==180 ? -arc.getRadiusX() : arc.getRadiusX());
         return "<path d=\"" +
-                " M " + mX + "," + mY +
+                "M " + mX + "," + mY +
                 " A " + arc.getRadiusX() + "," + arc.getRadiusY() +
                 " 0 0 1 " + aX + "," + aY +
                 "\" stroke-width=\"" + arc.getStrokeWidth() + "px\"" +
