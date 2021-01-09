cbdc12b7d7a869340db1d8c68851b6e8ae640cb8
Tahmid A. Khan <60913202+ktahmid@users.noreply.github.com>
2021-01-09 21:17:24 +0600

    File saving fixed: ShapeStringGenerator now produces correct co-ordinates.

--- src/ShapeStringGenerator.java
+++ src/ShapeStringGenerator.java
@@ -17,39 +17,43 @@ public class ShapeStringGenerator {
 
     public static String getStr(Line line, int w, int h) {
         return "line," +
-                convert(line.getStartX(), w) + "," +
-                convert(line.getStartY(), h) + "," +
-                convert(line.getEndX(), w) + "," +
-                convert(line.getEndY(), h);
+                translateX(line.getStartX(), w) + "," +
+                translateY(line.getStartY(), h) + "," +
+                translateX(line.getEndX(), w) + "," +
+                translateY(line.getEndY(), h);
     }
 
     public static String getStr(Polyline polyline, int w, int h) {
         StringBuilder str = new StringBuilder("polyline");
         for (int i = 0; i < polyline.getPoints().size(); i += 2) {
-            str.append(",").append(convert(polyline.getPoints().get(i), w));
-            str.append(",").append(convert(polyline.getPoints().get(i + 1), h));
+            str.append(",").append(translateX(polyline.getPoints().get(i), w));
+            str.append(",").append(translateX(polyline.getPoints().get(i + 1), h));
         }
         return str.toString();
     }
 
     public static String getStr(Arc arc, int w, int h) {
         return "arc," +
-                convert(arc.getCenterX(), w) + "," +
-                convert(arc.getCenterY(), h) + "," +
-                convert(arc.getRadiusX(), w) + "," +
-                convert(arc.getRadiusY(), h) + "," +
+                translateX(arc.getCenterX(), w) + "," +
+                translateY(arc.getCenterY(), h) + "," +
+                translateX(arc.getRadiusX(), w) + "," +
+                translateY(arc.getRadiusY(), h) + "," +
                 arc.getStartAngle();
     }
 
     public static String getStr(Ellipse ellipse, int w, int h) {
         return "ellipse," +
-                convert(ellipse.getCenterX(), w) + "," +
-                convert(ellipse.getCenterY(), h) + "," +
-                convert(ellipse.getRadiusX(), w) + "," +
-                convert(ellipse.getRadiusY(), h);
+                translateX(ellipse.getCenterX(), w) + "," +
+                translateY(ellipse.getCenterY(), h) + "," +
+                translateX(ellipse.getRadiusX(), w) + "," +
+                translateY(ellipse.getRadiusY(), h);
     }
 
-    private static double convert(double xOrY, int widthORHeight) {
-        return xOrY - widthORHeight / 2;
+    private static double translateX(double x, int width) {
+        return x - width/2;
+    }
+
+    private static double translateY(double y, int height) {
+        return height/2 - y;
     }
 }
--- src/ShapeStringParser.java
+++ src/ShapeStringParser.java
@@ -6,40 +6,44 @@ public class ShapeStringParser {
         switch (tokens[0]) {
             case "line":
                 return new Line(
-                        convert(tokens[1], w),
-                        convert(tokens[2], h),
-                        convert(tokens[3], w),
-                        convert(tokens[4], h)
+                        translateX(tokens[1], w),
+                        translateY(tokens[2], h),
+                        translateX(tokens[3], w),
+                        translateY(tokens[4], h)
                 );
             case "polyline":
                 Polyline polyline = new Polyline();
                 for (int i = 1; i < tokens.length; i += 2) {
-                    polyline.getPoints().add(convert(tokens[i], w));
-                    polyline.getPoints().add(convert(tokens[i+1], h));
+                    polyline.getPoints().add(translateX(tokens[i], w));
+                    polyline.getPoints().add(translateY(tokens[i+1], h));
                 }
                 return polyline;
             case "arc":
                 return new Arc(
-                        convert(tokens[1], w),
-                        convert(tokens[2], h),
-                        convert(tokens[3], w),
-                        convert(tokens[4], h),
+                        translateX(tokens[1], w),
+                        translateY(tokens[2], h),
+                        translateX(tokens[3], w),
+                        translateY(tokens[4], h),
                         Double.parseDouble(tokens[5]),
                         90
                 );
             case "ellipse":
                 return new Ellipse(
-                        convert(tokens[1], w),
-                        convert(tokens[2], h),
-                        convert(tokens[3], w),
-                        convert(tokens[4], h)
+                        translateX(tokens[1], w),
+                        translateY(tokens[2], h),
+                        translateX(tokens[3], w),
+                        translateY(tokens[4], h)
                 );
             default:
                 return new Shape() {};
         }
     }
 
-    private static double convert(String xOrY, int widthORHeight) {
-        return Double.parseDouble(xOrY) + widthORHeight / 2;
+    private static double translateX(String x, int width) {
+        return Double.parseDouble(x) + width/2;
+    }
+
+    private static double translateY(String y, int height) {
+        return height/2 - Double.parseDouble(y);
     }
 }
