b13d54c720bf38f4980b806bcb50764c4d27e66d
Tahmid A. Khan <60913202+ktahmid@users.noreply.github.com>
2021-01-10 11:10:23 +0600

    Update DrawingHelper: remove diagnostic printings.

--- src/DrawingHelper.java
+++ src/DrawingHelper.java
@@ -83,28 +83,24 @@ public class DrawingHelper {
         Line line = new Line(x1,y1, x2,y2);
         line.setStrokeWidth(STROKE_WIDTH);
         canvas.getChildren().add(line);
-        System.out.println(line.toString());
     }
 
     public void drawPolyline(Pane canvas, Polyline polyline) {
         Polyline newPolyline = polyline;
         newPolyline.setStroke(Color.BLACK); newPolyline.setStrokeWidth(STROKE_WIDTH);
         canvas.getChildren().add(newPolyline);
-        System.out.println(newPolyline.toString());
     }
 
     public void drawArc(Pane canvas, double centerX,double centerY, double radiusX,double radiusY, double startAngle) {
         Arc arc = new Arc(centerX, centerY, radiusX, radiusY, startAngle, 90);
         arc.setStroke(Color.BLACK); arc.setStrokeWidth(STROKE_WIDTH); arc.setFill(null);
         canvas.getChildren().add(arc);
-        System.out.println(arc.toString());
     }
 
     public void drawEllipse(Pane canvas, double centerX, double centerY, double radiusX, double radiusY) {
         Ellipse ellipse = new Ellipse(centerX,centerY, radiusX,radiusY);
         ellipse.setStroke(Color.BLACK); ellipse.setStrokeWidth(STROKE_WIDTH); ellipse.setFill(null);
         canvas.getChildren().add(ellipse);
-        System.out.println(ellipse.toString());
     }
 
     public void drawDot(Pane pane, double x, double y, Color color) {
